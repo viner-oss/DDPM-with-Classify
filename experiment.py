a3eb6ddc5ada8c7da325005eba17a7aaa30c8805
viner-oss <2895724421@qq.com>
2025-04-25 18:20:48 +0800

    Update experiment.py

--- experiment.py
+++ experiment.py
@@ -1,4 +1,3 @@
-import random
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
@@ -6,36 +5,19 @@ import torch
 # 基本参数
 T = 100
 batch_size = 64
-simulate_image = torch.randn([batch_size,3,32,32])
 bat = 32
-
-# time_step = torch.randint(0, T, (batch_size,)).long()
-# label_step = torch.randint(0, 10, (batch_size, )).long()
-#
-# time_label_encoder = DDPM.Embedding_Util(10, T, batch_size)
-# ddpm = DDPM.Diffusion(T)
-#
-# image, real_noise = ddpm.forward(simulate_image, time_step)
-# image = time_label_encoder.time_embedding(image, time_step)
-# image = time_label_encoder.label_embedding(image, label_step)
-#
-# model = DDPM.UNet()
-# predict_noise, _ = model(image)
 
 def Gaussian(x, mean, std):
     return (1/std*np.sqrt(2*np.pi))*np.exp(-(x-mean)**2/2*(std**2))
 
 
-
-# # 从一个批次中随机选出一张图
-# real_noise = real_noise.numpy()[bat,:,:,:]
-# predict_noise = predict_noise.detach().numpy()[bat,:,:,:]
-
 """
 显示预测噪声 与 真实噪声的概率密度曲线
 训练阶段可以尝试使用
 """
-def graph_show(real_np_noise, predict_np_noise):
+def graph_show(real_noise, predict_noise):
+    real_np_noise = real_noise.numpy()[bat,:,:,:]
+    predict_np_noise = predict_noise.detach().numpy()[bat, :, :, :]
     plt.figure(num=1, label='real || predict', figsize=(6,6))
     real_mean = np.mean(real_np_noise[:,:,:])
     predict_mean = np.mean(predict_np_noise[:,:,:])
@@ -111,7 +93,7 @@ def graph_show(real_np_noise, predict_np_noise):
     ax3.yaxis.set_ticks_position('left')
     ax3.spines['left'].set_position(('data', -5))
     ax3.spines['bottom'].set_position(('data', 0))
-    plt.xticks(np.arange(-5, 5, 0.5))
+    plt.xticks(np.arange(-5, 5, 1))
     plt.xlim(-5, 5)
     plt.yticks(np.arange(0, 5, 1))
     plt.ylim(0, 4)
@@ -138,17 +120,17 @@ def graph_show(real_np_noise, predict_np_noise):
                  arrowprops=dict(arrowstyle='->', connectionstyle='arc3, rad=.2'))
     plt.legend()
 
-    plt.figure(num=3, label='image')
-    plt.subplot(1,2,1)
-    real_scale = (real_np_noise - np.min(real_np_noise)) / (np.max(real_np_noise) - np.min(real_np_noise)) * 255
-    real_clip = np.clip(real_scale, 0, 255).astype(np.uint8)
-    plt.imshow(np.swapaxes(np.swapaxes(real_clip[:,:,:], axis1=0, axis2=1), axis1=1, axis2=2), cmap='bone', interpolation='nearest', origin='upper')
-
-    plt.subplot(1,2,2)
-    predict_scale = (predict_np_noise - np.min(predict_np_noise)) / (np.max(predict_np_noise) - np.min(predict_np_noise)) * 255
-    predict_clip = np.clip(predict_scale, 0, 255).astype(np.uint8)
-
-    plt.imshow(np.swapaxes(np.swapaxes(predict_clip[:,:,:], axis1=0, axis2=1), axis1=1, axis2=2), cmap='bone', interpolation='nearest', origin='upper')
+    # plt.figure(num=3, label='image')
+    # plt.subplot(1,2,1)
+    # real_scale = (real_np_noise - np.min(real_np_noise)) / (np.max(real_np_noise) - np.min(real_np_noise)) * 255
+    # real_clip = np.clip(real_scale, 0, 255).astype(np.uint8)
+    # plt.imshow(np.swapaxes(np.swapaxes(real_clip[:,:,:], axis1=0, axis2=1), axis1=1, axis2=2), cmap='bone', interpolation='nearest', origin='upper')
+    #
+    # plt.subplot(1,2,2)
+    # predict_scale = (predict_np_noise - np.min(predict_np_noise)) / (np.max(predict_np_noise) - np.min(predict_np_noise)) * 255
+    # predict_clip = np.clip(predict_scale, 0, 255).astype(np.uint8)
+    #
+    # plt.imshow(np.swapaxes(np.swapaxes(predict_clip[:,:,:], axis1=0, axis2=1), axis1=1, axis2=2), cmap='bone', interpolation='nearest', origin='upper')
 
 
 
@@ -166,14 +148,18 @@ def imshow_image(x, i, epcho, test_step):
     plt.subplot(4,4,i)
     image = np.swapaxes(np.swapaxes(image_clip[:,:,:], axis1=0, axis2=1), axis1=1, axis2=2)
     plt.imshow(image, cmap='bone', interpolation='nearest', origin='upper')
-    plt.imsave(f'D:\python\Segmentation\experiment\denoise_process{test_step}_{epcho}_{i}.png', arr=image)
+    # plt.imsave(f'D:\python\Segmentation\experiment\denoise_process{test_step}_{epcho}_{i}.png', arr=image)
 
 
 
 if __name__ == "__main__":
-    # graph_show(real_np_noise=real_noise, predict_np_noise=predict_noise)
-    plt.figure(label='prediction')
-    for i in range(100):
-        imshow_image(simulate_image, i+1, 1)
+    simulate_noise = torch.randn([batch_size, 3, 32, 32])
+    simulate_predict_image = torch.randn([batch_size, 3, 32, 32])
+
+    graph_show(real_noise=simulate_noise, predict_noise=simulate_predict_image)
+
+    # plt.figure(label='prediction')
+    # for i in range(16):
+    #     imshow_image(simulate_image, i+1, 5, 100)
 
     plt.show()
